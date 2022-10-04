e51d00f61a069fef3acf78b70fc998be526c4b90
Arash <47273556+arashnrim@users.noreply.github.com>
2022-10-04 12:33:32 +0800

    feat: Implement basic navigation and bottom navigation bar

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from "./src/Home"
+import Services from "./src/Services"
+import Articles from "./src/Articles"
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons"
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home"
+          } else if (route.name === "Services") {
+            iconName = "pill"
+          } else if (route.name === "Articles") {
+            iconName = "post"
+          }
+
+          return <MaterialCommunityIcons name={iconName} size={size} color={color} />
+        }
+      })}>
+        <Tab.Screen name="Home" component={Home}></Tab.Screen>
+        <Tab.Screen name="Services" component={Services}></Tab.Screen>
+        <Tab.Screen name="Articles" component={Articles}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ src/Services.jsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+
+const Services = () => {
+    return <View style={{height: "100%", "width": "100%"}}><Text>Services</Text></View>
+}
+
+export default Services;
