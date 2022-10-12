998e62da8264d55c8b390ed51d5fe62407a201f7
avicity7 <74177090+avicity7@users.noreply.github.com>
2022-10-12 21:46:22 +0800

    fix: Delete

--- src/screens/Articles.jsx
+++
@@ -1,7 +0,0 @@
-import { View, Text } from "react-native";
-
-const Articles = () => {
-    return <View style={{height: "100%", "width": "100%"}}><Text>Articles</Text></View>
-}
-
-export default Articles;
--- src/screens/Home.jsx
+++
@@ -1,7 +0,0 @@
-import { View, Text } from "react-native";
-
-const Home = () => {
-    return <View style={{height: "100%", "width": "100%"}}><Text>Home</Text></View>
-}
-
-export default Home;
