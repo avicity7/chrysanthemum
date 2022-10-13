e4b4110f8933d8c8a2ccd65fac1607fa518fe557
avicity7 <74177090+avicity7@users.noreply.github.com>
2022-10-13 13:44:55 +0800

    fix: Commit files

--- src/screens/Services.jsx
+++ src/screens/Services.jsx
@@ -3,25 +3,8 @@ import { useState } from "react";
 import globalStyles from "../styles/global";
 import Button from "../components/Button";
 
-
-const Services = () => {
-    const [userData,setData] = useState("test");
-
-    return (
-    <View style={globalStyles.container}>
-        <Button icon="sort" customStyle={{marginBottom: 8}} title={"Triage"} onPress={() => {
-            let transactions = getTransactions("0xD1B59E30Ce1Cea72A607EBf6141109bce89207E8",setData);
-        }} />
-        <View style={style.split}>
-            <Button icon="archive-edit" customStyle={[style.occupy, {marginRight: 8}]} title={"Update Records"} onPress={() => {/* TODO: Add functionality */}} />
-            <Button icon="archive-search" customStyle={[style.occupy, {marginLeft: 8}]} title={"View Records"} onPress={() => {/* TODO: Add functionality */}} />
-        </View>
-        <Text>{userData}</Text>
-    </View>)
-}
-
 const getTransactions = async (wallet,setData)=>{
-    const apiURL = 'http://localhost:5000';
+    const apiURL = 'http://13.212.100.69:5000';
     await fetch(apiURL + "/safePublish/getTransactions", {
         method: "POST",
         headers: {
@@ -35,6 +18,40 @@ const getTransactions = async (wallet,setData)=>{
     .then((data => {
         setData(data.transactions)
     }))
+}
+
+const sendTriage = async (wallet,setData)=>{
+    const apiURL = 'http://13.212.100.69:5000';
+    await fetch(apiURL + "/safePublish/sendTriage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userAddress: wallet,
+        }),
+    })
+    .then((response) => response.json())
+    .then((data => {
+        setData(data.key)
+    }))
+}
+
+
+const Services = () => {
+    const [userData,setData] = useState("test");
+
+    return (
+    <View style={globalStyles.container}>
+        <Button icon="sort" customStyle={{marginBottom: 8}} title={"Triage"} onPress={() => {
+            sendTriage("0xD1B59E30Ce1Cea72A607EBf6141109bce89207E8",setData);
+        }} />
+        <View style={style.split}>
+            <Button icon="archive-edit" customStyle={[style.occupy, {marginRight: 8}]} title={"Update Records"} onPress={() => {/* TODO: Add functionality */}} />
+            <Button icon="archive-search" customStyle={[style.occupy, {marginLeft: 8}]} title={"View Records"} onPress={() => {getTransactions("0xD1B59E30Ce1Cea72A607EBf6141109bce89207E8",setData)}} />
+        </View>
+        <Text>{userData}</Text>
+    </View>)
 }
 
 const style = StyleSheet.create({
